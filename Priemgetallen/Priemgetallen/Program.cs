6b0dea8b1698cf2ef1aac7a7748624542c8618cf
LorenzoLatinnePXL <lorenzo.latinne@student.pxl.be>
2024-11-04 10:07:06 +0100

    Exercise finished.

---
+++ Priemgetallen/Priemgetallen/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priemgetallen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+                bool isPrime = true;
+
+                if (i <= 1)
+                {
+                    isPrime = false;
+                } 
+                else
+                {
+                    for (int j = 2; j * j <= i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (i % 10 != 0)
+                {
+                    if (isPrime)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{i}\t");
+                    } else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"{i}\t");
+                    }
+                } 
+                else
+                {
+                    if (isPrime)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{i}\t");
+                    } else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"{i}\t");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
